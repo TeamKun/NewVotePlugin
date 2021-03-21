ab842d0c8318e46e96545bb471a5ebc12f1c7aec
yanaaaaa/やなー <76870837+yanayana8721@users.noreply.github.com>
2021-03-21 17:57:26 +0900

    Add files via upload

--- src/main/java/net/kunmc/lab/newvote/ScoreBoardLogic.java
+++ src/main/java/net/kunmc/lab/newvote/ScoreBoardLogic.java
@@ -16,11 +16,11 @@ public class ScoreBoardLogic {
     static void setVoteStatus(int pattern, Player sender){
         //投票開始時の処理
         if(pattern==0){
-            sender.setPlayerListName(sender.getName()+ChatColor.GRAY+"  ×");
+            sender.setPlayerListName(ChatColor.GRAY+"× "+ChatColor.WHITE+sender.getName());
         }
         //投票時
         else if(pattern==1){
-            sender.setPlayerListName(sender.getName()+ChatColor.GOLD+"  ✓");
+            sender.setPlayerListName(ChatColor.GOLD+"✓ "+ChatColor.WHITE+sender.getName());
         }
         //投票終了時
         else{
